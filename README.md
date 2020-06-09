702a1ee948cb7fecedacf32d79c6c48d7b6152af
Daniel Rodrigues <danielrodrigues@wanngi.com>
2020-06-09 14:21:09 +0100

    Add initial implementation

---
+++ README.md
@@ -0,0 +1,2 @@
+# node-api-online-booking-system
+A online-booking-system for easy and faster project initialisation.
---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import router from 'router';
+
+export default () => {
+  const app = new Koa();
+
+  app
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .use(bodyParser());
+
+  return app;
+};
